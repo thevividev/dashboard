21380e8fa30dc6db5af44b88132a6cacd5dcaeb7
brethesevin <bretheskevin@gmail.com>
2023-03-07 11:27:06 +0000

    produ

---
+++ prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d "dist" ]; then
+  rm -rf dist
+fi
+
+npm install
+npm run build
+
+sudo rm -rf /var/www/html/thevivi-dashboard/
+sudo mv dist/dashboard dist/thevivi-dashboard
+sudo cp -r dist/thevivi-dashboard/ /var/www/html/
